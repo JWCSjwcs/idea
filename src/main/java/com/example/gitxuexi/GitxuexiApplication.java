1a577246789dae13ef6e80ea80788bf3db78cdc8
anxiaomeng <1457170495@qq.com>
2021-08-25 22:33:43 +0800

    c1

---
+++ src/main/java/com/example/gitxuexi/GitxuexiApplication.java
@@ -0,0 +1,21 @@
+package com.example.gitxuexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class GitxuexiApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("dad");
+        System.out.println("dad");
+        System.out.println("3");
+        System.out.println("gai2");
+        System.out.println("adad");
+        System.out.println("c1");
+
+    }
+}
